9d934e9c9b8c71110919509c212b32d66651f067
SanketKumar21 <kumarsanket199724@gmail.com>
2022-10-13 01:01:36 +0530

    assignment

---
+++ DAY3/number1.js
@@ -0,0 +1,10 @@
+let a = 12
+let b = 35
+let c = 45
+if (a>b && a>c){
+  console.log(a,"is greatest")
+} else if (b>a && b>c) {
+  console.log(b,"is greatest")  
+} else if (c>a && c>b) {
+  console.log(c,"is greatest")
+}
---
+++ DAY3/week1.js
@@ -0,0 +1,21 @@
+let day = "Fri"
+
+switch (day) {
+
+  case "Sun": console.log("Sunday");
+    break;
+  case "Mon": console.log("Monday");
+    break;
+  case "Tue": console.log("Tuesday")
+    break;
+  case "Wed": console.log("Wednesday")
+    break;
+  case "Thur":console.log("Thursday")
+    break;
+  case "Fri": console.log("Friday")
+    break;
+  case "Sat": console.log("Saturday")
+    break;
+  default: console.log("Input Error")
+  
+}
